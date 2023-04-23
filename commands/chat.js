c2ff68a836d975a3805f6346770ebc28b5bf49c9
Elian <eliangerardiso@gmail.com>
2023-04-23 01:29:24 -0600

    Added voice confirmation when speech activation

--- commands/chat.js
+++ commands/chat.js
@@ -48,7 +48,7 @@ module.exports = {
     },
     async executeVoice(content, msg, client) {
         const { ChatGPTUnofficialProxyAPI } = await import('chatgpt');
-
+        say("Procesando la pregunta de "+msg.author.username);
         const api = new ChatGPTUnofficialProxyAPI({
             accessToken: client.config.gptAccessToken,
             apiReverseProxyUrl: "https://ai.fakeopen.com/api/conversation"
